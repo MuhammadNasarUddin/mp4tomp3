c1bee5b609f04fdb9b0348d402d2b144aa3f07c7
nasar <muhammadnasar012@gmail.com>
2023-12-18 10:01:53 +0500

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-import streamlit as st
+from flask import Flask, render_template, request,send_file
 from moviepy.editor import *
 import os
+
+app = Flask(__name__)
 
 def convert_mp4_to_mp3(uploaded_file, output_file):
     with open(output_file, "wb") as out_file:
@@ -10,21 +12,22 @@ def convert_mp4_to_mp3(uploaded_file, output_file):
     audio = video.audio
     audio.write_audiofile(output_file.replace(".mp3", "_converted.mp3"))
 
-def main():
-    st.title("MP4 to MP3 Converter")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            try:
+                output_file_path = "output_audio.mp3"
+                convert_mp4_to_mp3(uploaded_file, output_file_path)
+                return render_template('mp4.html', message="Conversion completed!", converted_file=output_file_path.replace(".mp3", "_converted.mp3"))
+            except Exception as e:
+                return render_template('mp4.html', error_message=str(e))
+    return render_template('mp4.html')
 
-    uploaded_file = st.file_uploader("Upload an MP4 file", type=["mp4"])
-
-    if uploaded_file is not None:
-        output_file_path = "output_audio.mp3"
-
-        convert_button = st.button("Convert to MP3")
-
-        if convert_button:
-            convert_mp4_to_mp3(uploaded_file, output_file_path)
-            st.success("Conversion completed!")
-
-            st.audio(output_file_path.replace(".mp3", "_converted.mp3"), format='audio/mp3')
+@app.route('/download/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    return send_file(filename, as_attachment=True)
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
